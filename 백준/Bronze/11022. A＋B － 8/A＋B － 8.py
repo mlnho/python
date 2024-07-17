50de9204773c86dc5365cbf865783ca697f1fb95
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-07-18 00:03:21 +0900

    [Bronze V] Title: A+B - 8, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+# 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+
+n = int(input())
+
+for i in range(n):
+    a,b = map(int,input().split())
+    print(f'Case #{i+1}: {a} + {b} = {a+b}')
