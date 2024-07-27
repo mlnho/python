cbce000d165c483367d42426cca7e75de6ea2ad1
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-07-27 16:34:14 +0900

    [Bronze V] Title: A+B - 4, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,10 +1,5 @@
 import sys
 
-read = sys.stdin.readline
-
-while True:
-    try:
-        a,b = map(int,read().split())
-        print(a+b)
-    except:
-        break
+for i in sys.stdin:
+    a, b = map(int, i.split())
+    print(a + b)
