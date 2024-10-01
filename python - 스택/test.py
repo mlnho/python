1bed0059e04f2ca734ad3f6aa1ec4c476ec1354c
mlnho <1104minho@naver.com>
2024-10-01 11:59:51 +0900

    BOJ-EX: 2024. 10. 1. 오전 11:59:50

--- python - 스택/test.py
+++ python - 스택/test.py
@@ -35,3 +35,37 @@ print()
 # # 스택 순회
 while st:
     print(st.pop(), end=' ')
+    
+    
+    
+    
+    
+# BOJ 10828 : 기본 스택
+
+import sys  # 시간초과가 났어서 sys로 수정
+
+N = int(sys.stdin.readline())
+stack = []
+
+for i in range(N):
+    lst = list(sys.stdin.readline().split())  # 입력 값 받기
+    if "push" in lst:
+        stack.append(lst[1])
+        # print('현재 stack :',stack)
+    elif "pop" in lst:
+        if len(stack) == 0:  # stack 이 빈 리스트일 경우에
+            print(-1)
+        else:
+            print(stack.pop())
+    elif "size" in lst:
+        print(len(stack))
+    elif "empty" in lst:
+        if len(stack) == 0:  # stack 이 빈 리스트일 경우에
+            print(1)
+        else:
+            print(0)
+    elif "top" in lst:
+        if len(stack) == 0:  # stack 이 빈 리스트일 경우에
+            print(-1)
+        else:
+            print(stack[-1])
