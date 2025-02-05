da907f3c1a53f70b934c1d398432b1e698295733
mlnho <1104minho@naver.com>
2025-02-05 22:17:32 +0900

    [baekjoon]10818

---
+++ 01.baekjoon/array/10818_최소,최대.py
@@ -0,0 +1,13 @@
+# N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+# 출력
+# 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+N = int(input())
+
+# 배열 선언
+array = list(map(int,input().split()))
+
+print(min(array),max(array))
+
+
